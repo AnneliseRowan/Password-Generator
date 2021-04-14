2191a9d17667d348f71e4189d07c0357076abccf
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-04-14 14:47:07 -0400

    shortening my code and taking out if else statements to just have if statements

--- script.js
+++ script.js
@@ -1,71 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var start; 
-var confirmNumber; 
-var confirmCharacter; 
-var confirmUpper;
-var confirmLower; 
-var choices; 
-
-
 const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];  
 const numbers = [1,2,3,4,5,6,7,8,9,0]; 
 const symbols = ["!","@","#","$","%","^","&","*","_","-","+","="]; 
 
+var start; 
+var conNum; 
+var conSym; 
+var conUpp;
+var conLow; 
+var options = []; 
+
 
 function generatePassword() {
-  var start = Number((prompt("What is your desired length? Choose between 8 and 128")));
+  var start = Number(prompt("What is your desired length? Choose between 8 and 128"));
 
   if (!start) {
     alert("This needs a value");
-  } else if (start <= 8 || start => 128) {
-    start = parseInt(prompt("You must choose between 8 and 128"))
+  } else if (start < 8 || start > 128) {
+    start = prompt("You must choose between 8 and 128");
   } else {
-    confirmNumber = confirm("Will this contain numbers?");
-    confirmSymbol = confirm("Will this contain special symbols?");
-    confirmUpper = confirm("Will this contain Uppercase letters?");
-    confirmLower = confirm("Will this contain Lowercase letters?"); 
+    conNum = confirm("Will this contain numbers?");
+    conSym = confirm("Will this contain special symbols?");
+    conUpp = confirm("Will this contain Uppercase letters?");
+    conLow = confirm("Will this contain Lowercase letters?"); 
   }
 
-  if (!confirmSymbol && !confirmNumber && !confirmUpper && !confirmLower) {
-    choices = alert("You must choose a criteria"); 
-  } else if (confirmSymbol && confirmNumber && confirmUpper && confirmLower) {
-    choices = symbols.concat(numbers, uppercase, lowercase);
-  } else if (confirmSymbol && confirmNumber && confirmUpper) {
-    choices = symbols.concat(numbers, lowercase);
-  } else if (confirmSymbol && confirmNumber && confirmLower) {
-    choices = symbols.concat(numbers, uppercase); 
-  } else if (confirmSymbol && confirmLower && confirmUpper) {
-    choices = symbols.concat(uppercase, lowercase); 
-  } else if (confirmNumber && confirmLower && confirmUpper) {
-    choices = numbers.concat(uppercase, lowercase); 
-  } else if (confirmSymbol && confirmNumber) {
-    choices = symbols.concat(numbers);
-  } else if (confirmSymbol && confirmLower) {
-    choices = symbols.concat(uppercase); 
-  } else if (confirmSymbol && confirmUpper) {
-    choices = symbols.concat(lowercase);
-  } else if (confirmLower && confirmUpper) {
-    choices = uppercase.concat(numbers); 
-  } else if (confirmLowercase && confirmUppercase) {
-    choices = uppercase.concat(lowercase);
-  } else if (confirmNumber && confirmUppercase) {
-    choices = numbers.concat(lowercase);
-  } else if (confirmSymbol) {
-    choices = symbols;
-  } else if (confirmNumber) {
-    choices = numbers;
-  } else if (confirmLowercase) {
-    choices = uppercase; 
+  if (conSym) {
+    options = options.concat(symbols); 
+  }
+  if (conNum) {
+    options = options.concat(numbers); 
+  }
+  if (conUpp) {
+    options = options.concat(uppercase); 
+  }
+  if (conLow) {
+    options = options.concat(lowercase); 
+  } else {
+    options = alert("You much choose a criteria")
   }
 
   var password = []; 
 
+  console.log(options);
+
   for (var i = 0; i < start; i++) {
-    var pickChoices = choices[Math.floor(Math.random() * choices.length)];
-    password.push(pickChoices); 
+    var pickOptions = options[Math.floor(Math.random() * options.length)];
+    password.push(pickOptions); 
   }
 
   var result = password.join(""); 
