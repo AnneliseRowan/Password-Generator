d9940b28a9b4d8d36487feafa63ac5aa929f40b8
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-04-14 14:54:08 -0400

    fixed a bug when wrong number put in

--- script.js
+++ script.js
@@ -1,72 +1,112 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];  
-const numbers = [1,2,3,4,5,6,7,8,9,0]; 
-const symbols = ["!","@","#","$","%","^","&","*","_","-","+","="]; 
-
-var start; 
-var conNum; 
-var conSym; 
+var generateBtn = document.querySelector('#generate');
+const uppercase = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+const lowercase = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '='];
+var start;
+var conNum;
+var conSym;
 var conUpp;
-var conLow; 
-var options = []; 
-
-
+var conLow;
+var options = [];
 function generatePassword() {
-  var start = Number(prompt("What is your desired length? Choose between 8 and 128"));
-
-  if (!start) {
-    alert("This needs a value");
-  } else if (start < 8 || start > 128) {
-    start = prompt("You must choose between 8 and 128");
-  } else {
-    conNum = confirm("Will this contain numbers?");
-    conSym = confirm("Will this contain special symbols?");
-    conUpp = confirm("Will this contain Uppercase letters?");
-    conLow = confirm("Will this contain Lowercase letters?"); 
+  var start = Number(prompt('What is your desired length? Choose between 8 and 128'));
+  function check() {
+    if (!start) {
+      alert('This needs a value');
+    } else if (start < 8 || start > 128) {
+      start = prompt('You must choose between 8 and 128');
+      check();
+    } else {
+      conNum = confirm('Will this contain numbers?');
+      conSym = confirm('Will this contain special symbols?');
+      conUpp = confirm('Will this contain Uppercase letters?');
+      conLow = confirm('Will this contain Lowercase letters?');
+    }
   }
-
+  check();
   if (conSym) {
-    options = options.concat(symbols); 
+    options = options.concat(symbols);
   }
   if (conNum) {
-    options = options.concat(numbers); 
+    options = options.concat(numbers);
   }
   if (conUpp) {
-    options = options.concat(uppercase); 
+    options = options.concat(uppercase);
   }
   if (conLow) {
-    options = options.concat(lowercase); 
+    options = options.concat(lowercase);
   } else {
-    options = alert("You much choose a criteria")
+    options = alert('You much choose a criteria');
   }
-
-  var password = []; 
-
+  var password = [];
   console.log(options);
-
   for (var i = 0; i < start; i++) {
     var pickOptions = options[Math.floor(Math.random() * options.length)];
-    password.push(pickOptions); 
+    password.push(pickOptions);
   }
-
-  var result = password.join(""); 
-  return result; 
+  var result = password.join('');
+  return result;
 }
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
+  var passwordText = document.querySelector('#password');
   passwordText.value = password;
 }
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
+generateBtn.addEventListener('click', writePassword);
